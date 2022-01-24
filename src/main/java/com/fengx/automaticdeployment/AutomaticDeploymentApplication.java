15683cff06999b720d6d58b0f98b18427a23afc8
fengxiao <1063371248@qq.com>
2022-01-24 13:49:13 +0800

    项目初始化

---
+++ src/main/java/com/fengx/automaticdeployment/AutomaticDeploymentApplication.java
@@ -0,0 +1,13 @@
+package com.fengx.automaticdeployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomaticDeploymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutomaticDeploymentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fengx/automaticdeployment/AutomaticDeploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fengx.automaticdeployment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomaticDeploymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
